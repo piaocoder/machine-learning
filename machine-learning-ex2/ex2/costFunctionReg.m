4e74233a898d8c1191af026f5c2dfd5e901e0187
piaocoder <piaocode@hotmail.com>
2016-03-13 10:49:18 +0800

    completed "Regularized Logistic Regression Cost" and " Regularized Logistic Regression Gradient"

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,7 +18,9 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
+theta1 = [0;theta(2:end)];    %theta(1)不参与正则化
+J= -1 * sum(y .* log(sigmoid(X*theta)) + (1 - y) .* log((1 - sigmoid(X*theta)))) / m  + lambda/(2*m) * theta1' * theta1;
+grad = (X' * (sigmoid(X*theta) - y))/ m + lambda/m * theta1;
 
 
 
