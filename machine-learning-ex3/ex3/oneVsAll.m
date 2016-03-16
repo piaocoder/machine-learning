9bff0ac951cceb1b610e2358012382e25a9f77ce
piaocoder <piaocode@hotmail.com>
2016-03-16 19:56:03 +0800

    One-vs-All Classifier Training

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -52,9 +52,16 @@ X = [ones(m, 1) X];
 
 
 
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
+initial_theta = zeros(n + 1, 1);
 
+for c = 1:num_labels
 
+	all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+	            initial_theta, options);
+
+end
 
 
 
