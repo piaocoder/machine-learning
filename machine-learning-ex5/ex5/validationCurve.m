94b43582edb897fa43526b5a0764dd88d723fd4b
piaocoder <piaocode@hotmail.com>
2016-04-03 12:16:20 +0800

    solve Validation Curve

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -41,10 +41,12 @@ error_val = zeros(length(lambda_vec), 1);
 
 
 
-
-
-
-
+for i = 1:length(lambda_vec)
+	lambda = lambda_vec(i);
+	theta = trainLinearReg(X, y,lambda);  % 用训练集训练出参数
+	error_train(i) = linearRegCostFunction(X, y,theta,0);
+	error_val(i) = linearRegCostFunction(Xval, yval,theta,0);           
+end
 
 
 
